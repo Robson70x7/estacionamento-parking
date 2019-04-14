d963ba41f8df7753df132d00dbe2d651b69d83d2
Robson Silva Brito de Moraes <robson.silva94@hotmail.com>
2019-04-13 23:05:54 -0300

    Finalizando configurações para heroku

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -1,11 +1,14 @@
 import os
+from decouple import config
+from dj_database_url import parse as dburl
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = '#$o(s6zlbmk7i5hj^+x7ew7orq(a%yaoi8$v)%tns*8uwgaq1z'
+SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','estacionamento-core.herokuapp.com/']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -47,11 +50,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'estacionamento.wsgi.application'
 
+default_dburl = 'sqlite:///'+ os.path.join(os.BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -80,3 +81,5 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(os.BASE_DIR, 'staticfiles')
