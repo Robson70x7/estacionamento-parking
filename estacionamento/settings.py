1446d7e4edaf84508b862c752b17dc8d02924115
Robson Silva Brito de Moraes <robson.silva94@hotmail.com>
2019-04-13 23:55:29 -0300

    Tentando Subir

--- estacionamento/settings.py
+++ estacionamento/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost','estacionamento-core.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost','estacionamento-garage.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
